0a483a5f7e11d5361b0428f09be9249844bdbfa1
Olzhas <olzhastoi26@gmail.com>
2023-04-24 23:10:09 +0600

    types 1

--- api/views.py
+++ api/views.py
@@ -52,8 +52,8 @@ class ClientCreateView(generics.GenericAPIView):
                 'email': client.email,
                 'city': client.city_id,
                 'phone': client.phone,
-                'refresh': str(refresh),
-                'access': str(refresh.access_token),
+                'refresh_token': str(refresh),
+                'access_token': str(refresh.access_token),
             }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
