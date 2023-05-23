9f6aa4b330330107c2094f049ba4b8646f5137e8
Drong0 <74701799+Drong0@users.noreply.github.com>
2023-05-23 16:40:13 +0600

    Update asgi.py

--- Diploma/asgi.py
+++ Diploma/asgi.py
@@ -6,10 +6,9 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-import chat.routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Diploma.settings')
 django_app = get_asgi_application()
-
+import chat.routing
 application = ProtocolTypeRouter(
     {
         "http": django_app, # Use the separate Django application for HTTP requests
